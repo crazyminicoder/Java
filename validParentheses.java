d3313b0b917696f2e41c5380b0bcb3550fd40f80
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2025-02-03 12:58:05 -0600

    update

--- validParentheses.java
+++ validParentheses.java
@@ -4,7 +4,6 @@ public class validParentheses {
     public boolean vp(String s) {
         Stack<Character> stack = new Stack<>();
 
-        
         stack.push(s.charAt(0));
         // System.out.println(stack.peek());
         for (int i = 1; i < s.length(); i++) {
@@ -20,6 +19,7 @@ public class validParentheses {
                     stack.push(s.charAt(i));
                 }
             } else if (s.charAt(i) == '}') {
+
                 if (stack.isEmpty()) {
 
                     return false;
@@ -58,7 +58,9 @@ public class validParentheses {
 
     public static void main(String args[]) {
         String s = "(){}}{";
+
         validParentheses v = new validParentheses();
+
         System.out.println(v.vp(s));
     }
 }
