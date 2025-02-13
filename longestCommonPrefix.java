2a6143b0b4fdc4ac1e53d545b405f76fc58ad62f
Syed Wali Uddin Quadri <69212896+crazyminicoder@users.noreply.github.com>
2025-02-13 12:03:53 -0600

    update

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -5,7 +5,9 @@ public class longestCommonPrefix {
         char ch;
         for (int i = 0; i < word.length(); i++) {
             ch = word.charAt(i);
+
             for (int j = 1; j < s.length; j++) {
+
                 if (s[j].charAt(i) != ch || i >= s[j].length()) {
                     return res;
                 }
@@ -17,6 +19,7 @@ public class longestCommonPrefix {
 
     public static void main(String args[]) {
         String[] str = { "flower", "flow", "flight" };
+
         longestCommonPrefix l = new longestCommonPrefix();
         String res = l.lcp(str);
         System.out.println(res);
